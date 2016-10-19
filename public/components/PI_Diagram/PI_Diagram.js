f54d95142aea4928f0749e22b5b32177e1ae590c
Anatoly V <anatoly.veshtemov@gmail.com>
2016-10-19 09:56:34 +0700

    start to make simulator

--- public/components/PI_Diagram/PI_Diagram.js
+++ public/components/PI_Diagram/PI_Diagram.js
@@ -6,119 +6,64 @@
             this._el = options.el;
             this._data = options.data;
 
-            this._svgList = {};
+            this._mainSVG = SVG('drawing').size(830, 530);
             this._widgets = {};
-
-            this.init();
 
             this.render();
         }
 
-        init() {
-            // Создаем главный холст
-            this._svgList.mainSVG = SVG('drawing').size(830, 530);
-            this._svgList.arrowSVG = this._svgList.mainSVG.nested();
-            this._svgList.pipeSVG = this._svgList.mainSVG.nested();
-            this._svgList.valveSVG = this._svgList.mainSVG.nested();
-            this._svgList.valve4waySVG = this._svgList.mainSVG.nested();
-            this._svgList.valve4waySVG = this._svgList.mainSVG.nested();
-            this._svgList.tankSVG = this._svgList.mainSVG.nested();
-            this._svgList.fanSVG = this._svgList.mainSVG.nested();
-            this._svgList.heaterSVG = this._svgList.mainSVG.nested();
-            this._svgList.relaySVG = this._svgList.mainSVG.nested();
-            this._svgList.sensorSVG = this._svgList.mainSVG.nested();
-        }
+
 
         render() {
 
-            // // Создаем главный холст
-            // this._svgList.mainSVG = SVG('drawing').size(830, 530);
-            //this._svgList.mainSVG.clear();
 
-            // this._svgList.mainSVG.clear();
-            this._svgList.arrowSVG.clear();
-            this._svgList.pipeSVG.clear();
-            this._svgList.valveSVG.clear();
-            this._svgList.valve4waySVG.clear();
-            this._svgList.valve4waySVG.clear();
-            this._svgList.tankSVG.clear();
-            this._svgList.fanSVG.clear();
-            this._svgList.heaterSVG.clear();
-            this._svgList.relaySVG.clear();
-            this._svgList.sensorSVG.clear();
-
-            // Создаем вложенный слой стрелок
-            //this._svgList.arrowSVG = this._svgList.mainSVG.nested();
+            this._mainSVG.clear();
 
             // Создаем список стрелок направления потока
             this._widgets.arrowList = [];
             PI_DiagramVieConfig.arrowDataList.forEach(val =>
-                this._widgets.arrowList.push(new ArrowWidget({el: this._svgList.arrowSVG, data: val})));
+                this._widgets.arrowList.push(new ArrowWidget({el:this._mainSVG, data: val})));
 
-            // Создаем вложенный слой труб
-            //this._svgList.pipeSVG = this._svgList.mainSVG.nested();
 
             // Создаем список труб
             this._widgets.pipeList = [];
             PI_DiagramVieConfig.pipeDataList.forEach(val =>
-                this._widgets.pipeList.push(new PipeWidget({el: this._svgList.pipeSVG, data: val})));
-
-
-            // Создаем вложенный слой клапанов
-            //this._svgList.valveSVG = this._svgList.mainSVG.nested();
+                this._widgets.pipeList.push(new PipeWidget({el: this._mainSVG, data: val})));
 
             // Создаем список клапанов
             this._widgets.valveList = [];
             PI_DiagramVieConfig.valveDataList.forEach(val =>
-                this._widgets.valveList.push(new ValveWidget({el: this._svgList.valveSVG, data: val})));
-
-            // Создаем вложенный слой 4х ходовых клапанов
-            // this._svgList.valve4waySVG = this._svgList.mainSVG.nested();
+                this._widgets.valveList.push(new ValveWidget({el: this._mainSVG, data: val})));
 
             // Создаем список клапанов
             this._widgets.valve4wayList = [];
             PI_DiagramVieConfig.valve4wayDataList.forEach(val =>
-                this._widgets.valve4wayList.push(new FourWayValveWidget({el: this._svgList.valve4waySVG, data: val})));
-
-            // Создаем вложенный слой колонн
-            // this._svgList.tankSVG = this._svgList.mainSVG.nested();
+                this._widgets.valve4wayList.push(new FourWayValveWidget({el: this._mainSVG, data: val})));
 
             // Создаем список колонн
             this._widgets.tankList = [];
             PI_DiagramVieConfig.tankDataList.forEach(val =>
-                this._widgets.tankList.push(new TankWidget({el: this._svgList.tankSVG, data: val})));
-
-            // Создаем вложенный слой нагнетателей
-            // this._svgList.fanSVG = this._svgList.mainSVG.nested();
+                this._widgets.tankList.push(new TankWidget({el: this._mainSVG, data: val})));
 
             // Создаем список нагнетателей
             this._widgets.fanList = [];
             PI_DiagramVieConfig.fanDataList.forEach(val =>
-                this._widgets.fanList.push(new FanWidget({el: this._svgList.fanSVG, data: val})));
-
-            // Создаем вложенный слой нагревателей
-            // this._svgList.heaterSVG = this._svgList.mainSVG.nested();
+                this._widgets.fanList.push(new FanWidget({el: this._mainSVG, data: val})));
 
             // Создаем список нагревателей
             this._widgets.heaterList = [];
             PI_DiagramVieConfig.heaterDataList.forEach(val =>
-                this._widgets.heaterList.push(new HeaterWidget({el: this._svgList.heaterSVG, data: val})));
-
-            // Создаем вложенный слой дискретных датчиков
-            // this._svgList.relaySVG = this._svgList.mainSVG.nested();
+                this._widgets.heaterList.push(new HeaterWidget({el: this._mainSVG, data: val})));
 
             // Создаем список дискретных датчиков
             this._widgets.relayList = [];
             PI_DiagramVieConfig.relayDataList.forEach(val =>
-                this._widgets.relayList.push(new RelayWidget({el: this._svgList.relaySVG, data: val})));
-
-            // Создаем вложенный слой аналоговых датчиков
-            // this._svgList.sensorSVG = this._svgList.mainSVG.nested();
+                this._widgets.relayList.push(new RelayWidget({el: this._mainSVG, data: val})));
 
             // Создаем список анаговых датчиков
             this._widgets.sensorList = [];
             PI_DiagramVieConfig.sensorDataList.forEach(val =>
-                this._widgets.sensorList.push(new SensorBarWidget({el: this._svgList.sensorSVG, data: {
+                this._widgets.sensorList.push(new SensorBarWidget({el: this._mainSVG, data: {
                     name: val.name,
                     x: val.x,
                     y: val.y,
